3d84657af79f1775fb1bfd3d9c02cf67e96f4a15
Bruno Teixeira <bruno_teixeira@hotmail.com>
2020-05-07 22:30:35 -0300

    add localStorage

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const ss = (el) => document.querySelectorAll(el);
 
 const lista = s('ul');
 let carros = [];
+if (localStorage.carros) {
+	carros = JSON.parse(localStorage.carros);
+	listagem();
+}
 
 // adicionando carro
 const botaoAdd = s('.add_btn');
@@ -13,14 +17,15 @@ function adicionar(e) {
 	verificarCampo();
 	const carro = s('#carro');
 	const placa = s('#placa');
-	if(!carro.value == '' && !placa.value == '') {
+	if (!carro.value == '' && !placa.value == '') {
 		const d = new Date;
 		const hora = Date.parse(d);
-		carros.push({modelo: carro.value, placa: placa.value, data: hora});
+		carros.push({ modelo: carro.value, placa: placa.value, data: hora });
 		carro.value = '';
 		placa.value = '';
+		localStorage.carros = JSON.stringify(carros);
 		listagem();
-	} 
+	}
 	else {
 		alert('Preencha os dados!!!');
 	}
@@ -28,39 +33,38 @@ function adicionar(e) {
 
 //listando carros
 function listagem() {
-	const lista = s('ul');
-	const li = document.createElement('li');
-
-	carros.forEach((carro) => {
-		const liCarro = `<span>${carro.modelo}</span>`;
-		const liPlaca = `<span>${carro.placa}</span>`;
-		const liHorario = `<span>${converterMs(carro.data)}</span>`;
-		const liBtn = `<button class="del_btn">Saida</button>`
-		li.innerHTML = liCarro + liPlaca + liHorario + liBtn;
+	lista.innerHTML = '';
+	carros.forEach(carro => {
+		const li = document.createElement('li');
+		li.innerHTML = `
+		<span>${carro.modelo}</span>
+		<span data-placa>${carro.placa}</span>
+		<span>${converterMs(carro.data)}</span>
+		<button class="del_btn" onclick="del(this)">Saida</button>
+		`;
 		lista.append(li);
-	});
-
-	//removendo carro
-	const botaoDel = ss('.del_btn');
-
-	botaoDel.forEach((item) => {
-		item.addEventListener('click', (carro) => {
-			const placa = carro.target.previousSibling.previousSibling.innerText;
-			carros = carros.filter((carro) => {
-				return carro.placa !== placa;
-			})
-			lista.innerHTML = '';
-			listagem();
-		});
 	});
 };
 
+//removendo carro
+function del(btn) {
+	const placa = btn.parentElement.querySelector('[data-placa]').innerText;
+	carros = carros.filter(carro => {
+		if(carro.placa === placa) {
+			hrEntrada = carro;
+		}
+		return carro.placa !== placa;
+	});
+	localStorage.carros = JSON.stringify(carros);
+	listagem();
+}
+
 //convertendo timestamp
 function converterMs(ms) {
-	const horas = `0${new Date(ms).getHours() }`.slice(-2);
+	const horas = `0${new Date(ms).getHours()}`.slice(-2);
 	const minutos = `0${new Date(ms).getMinutes()}`.slice(-2);
 
-	return  `${horas}:${minutos}`;
+	return `${horas}:${minutos}`;
 }
 
 //verificar campo vazio e mostar(borda vermelha)
@@ -71,7 +75,7 @@ function verificarCampo() {
 	});
 
 	function verificar() {
-		if(!this.value) {
+		if (!this.value) {
 			this.style.border = '1px solid #e54';
 		} else {
 			this.style.border = 'none'
